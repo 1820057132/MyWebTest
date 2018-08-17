6f9773f61c7dc4447f5ae1340e9dab7a6ce4e881
whd <1820057132@qq.com>
2018-08-17 14:11:00 +0800

    test

--- src/com/neusoft/servletTest/Main.java
+++ src/com/neusoft/servletTest/Main.java
@@ -13,6 +13,7 @@ public class Main {
         List<UserBean> list = new ArrayList<>();
         for (UserBean user : list){
             user.getSqlSex();
+            System.out.println("");
         }
 
     }
